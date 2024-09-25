d43b18bd0f63d226b94d1fcc516d69b3b54c8f88
Egor Onekeev <sergeev_e_k@mail.ru>
2024-09-25 20:38:30 +0300

    addTests

--- go.mod
+++ go.mod
@@ -3,20 +3,24 @@ module exT
 go 1.22.4
 
 require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/mattn/go-sqlite3 v1.14.23
 	github.com/mrRestyling/protos v0.0.1
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/crypto v0.27.0
 	google.golang.org/grpc v1.66.2
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
--- tests/auth_register_login_test.go
+++ tests/auth_register_login_test.go
@@ -3,6 +3,12 @@ package tests
 import (
 	"exT/tests/suite"
 	"testing"
+
+	"github.com/brianvoe/gofakeit"
+	"github.com/golang-jwt/jwt/v5"
+	ssov1 "github.com/mrRestyling/protos/proto/sso"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -18,5 +24,54 @@ func TestRegisterLogin_Login_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
 
 	// генерация случайного логина и пароля
+	// go get github.com/brianvoe/gofakeit/v6
 
+	email := gofakeit.Email()
+
+	pass := randomFakePassword()
+
+	// используем клиент, который есть в нашем suite
+
+	respReg, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
+		Email:    email,
+		Password: pass,
+	})
+
+	require.NoError(t, err) // аналог, который не останавливает тест -> assert.Equal(t,err)
+	assert.NotEmpty(t, respReg.GetUserId())
+
+	//Вызываем функцию логин
+	respLogin, err := st.AuthClient.Login(ctx, &ssov1.LoginRequest{
+		Email:    email,
+		Password: pass,
+		AppId:    appID,
+	})
+	require.NoError(t, err)
+
+	// Первое - получаем токен
+	token := respLogin.GetToken()
+	require.NotEmpty(t, token)
+
+	// Второе - пытаемся парсить этот токен
+	// appSecret  = "test-secret"
+	tokenParsed, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return []byte(appSecret), nil
+	})
+	require.NoError(t, err)
+
+	// Проверяем, что он проходит валидацию
+	claims, ok := tokenParsed.Claims.(jwt.MapClaims)
+	assert.True(t, ok)
+
+	// Проверяем, что в токене содержится корректная информация
+	assert.Equal(t, respReg.GetUserId(), int64(claims["uid"].(float64))) // особенности хранения данных в MapClaims
+	assert.Equal(t, email, claims["email"].(string))
+	assert.Equal(t, appID, int(claims["app_id"].(float64)))
+
+	// Проверяем, что время истечения токена совпадает с ожидаемым
+
+}
+
+func randomFakePassword() string {
+	return gofakeit.Password(true, true, true, true, false, passDefaultLen)
 }
